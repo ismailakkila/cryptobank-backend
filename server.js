28fc8c34e0a9a5f604ca61fda914129d226ce39f
ismailakkila <ismailakkila@gmail.com>
2019-01-20 14:28:10 +0400

    troubleshooting

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ mongodb.MongoClient.connect(process.env.DATABASEURI, {useNewUrlParser: true})
       app.use(express.static(indexPath));
       app.use(session({
         cookieParser: cookieParser,
-        key: "connect.sid",
+        name: "connect.sid",
         secret: process.env.SHARED_SECRET,
         resave: true,
         saveUninitialized: true,
@@ -91,7 +91,7 @@ mongodb.MongoClient.connect(process.env.DATABASEURI, {useNewUrlParser: true})
 
       io.use(passportSocketIo.authorize({
         cookieParser: cookieParser,
-        name: "connect.sid",
+        key: "connect.sid",
         secret: process.env.SHARED_SECRET,
         success: function(data, accept) {
           console.log(moment().toISOString() + " - [SocketIO] username: " + data.user.username + " connected and authenticated - session ID: " + data.sessionID);
