c1fe8c408396fc32409f7e83100d2773171db7b3
ismailakkila <ismailakkila@gmail.com>
2019-01-20 15:27:31 +0400

    troubleshooting

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ mongodb.MongoClient.connect(process.env.DATABASEURI, {useNewUrlParser: true})
         store: store,
         cookie: {
           httpOnly: true,
-          secure: true,
+          secure: false,
           maxAge: Date.now() + (30 * 86400 * 1000),
         }
       }));
